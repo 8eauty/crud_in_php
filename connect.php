6b9c54dd9940b90b87f0f045151d6c7bb388f1f9
8eauty <106524498+8eauty@users.noreply.github.com>
2023-01-24 21:12:03 +0530

    connect.php

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+$server="localhost";
+$username="root";
+$password="";
+ $database="crud";
+
+$con=mysqli_connect($server,$username,$password,$database);
+if($con){
+    echo " ";
+}else{
+    die(mysqli_connect_error());
+}
+?>
